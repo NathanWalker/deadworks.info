9d4b72fd6ca303df991440fd4d024bec73377b4a
Nathan Walker <nathan.walker@bizbuilt.com>
2012-11-29 21:30:20 -0800

    fixed setup

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var deadworksApp = angular.module('deadworksApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+deadworksApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
