b95d2a935020f515d19da51ba9b094432b79e8ff
Nathan Walker <nathan.walker@bizbuilt.com>
2012-11-29 21:19:52 -0800

    Initial bootstrap setup with angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var deadworks.infoApp = angular.module('deadworks.infoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
